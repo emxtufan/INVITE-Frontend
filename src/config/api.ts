382b8ec29db8f962d1e8f531101dcbf1a6ec8eae
emxtufan <emxtufan@icloud.com>
2026-04-01 16:53:14 +0300

    up

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,3 +1,3 @@
 
-const VITE_API_URL = "https://event-smart-assistant.com/api"
-export const API_URL = VITE_API_URL || "https://event-smart-assistant.com/api";
+const VITE_API_URL = "https://api.event-smart-assistant.com/api"
+export const API_URL = VITE_API_URL || "https://api.event-smart-assistant.com/api";
